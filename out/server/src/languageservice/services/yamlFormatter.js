c75950a6f92e015a7c65c7b2983e5f9f36f0a3da
Joseph Ward <joe@studiomvp.com>
2023-03-28 17:19:40 -0600

    adding build outptu

---
+++ out/server/src/languageservice/services/yamlFormatter.js
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Copyright (c) Adam Voss. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.YAMLFormatter = void 0;
+class YAMLFormatter {
+    constructor() {
+        this.formatterEnabled = false;
+    }
+    configure(shouldFormat) {
+    }
+    format(document, options) {
+        return [];
+    }
+}
+exports.YAMLFormatter = YAMLFormatter;
+//# sourceMappingURL=yamlFormatter.js.map
