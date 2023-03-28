21567d69c1410c090c630fb07dd36827b73983f7
Joe Ward <84737340+mvp-joe@users.noreply.github.com>
2023-03-28 16:54:47 -0600

    removing prettier

--- src/languageservice/services/yamlFormatter.ts
+++ src/languageservice/services/yamlFormatter.ts
@@ -7,48 +7,18 @@
 
 import { Range, Position, TextEdit, FormattingOptions } from 'vscode-languageserver-types';
 import { CustomFormatterOptions, LanguageSettings } from '../yamlLanguageService';
-import * as prettier from 'prettier';
-import { Options } from 'prettier';
-import * as parser from 'prettier/parser-yaml';
+// import * as prettier from 'prettier';
+// import { Options } from 'prettier';
+// import * as parser from 'prettier/parser-yaml';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
 export class YAMLFormatter {
-  private formatterEnabled = true;
+  private formatterEnabled = false;
 
   public configure(shouldFormat: LanguageSettings): void {
-    if (shouldFormat) {
-      this.formatterEnabled = shouldFormat.format;
-    }
   }
 
   public format(document: TextDocument, options: FormattingOptions & CustomFormatterOptions): TextEdit[] {
-    if (!this.formatterEnabled) {
-      return [];
-    }
-
-    try {
-      const text = document.getText();
-
-      const prettierOptions: Options = {
-        parser: 'yaml',
-        plugins: [parser],
-
-        // --- FormattingOptions ---
-        tabWidth: (options.tabWidth as number) || options.tabSize,
-
-        // --- CustomFormatterOptions ---
-        singleQuote: options.singleQuote,
-        bracketSpacing: options.bracketSpacing,
-        // 'preserve' is the default for Options.proseWrap. See also server.ts
-        proseWrap: 'always' === options.proseWrap ? 'always' : 'never' === options.proseWrap ? 'never' : 'preserve',
-        printWidth: options.printWidth,
-      };
-
-      const formatted = prettier.format(text, prettierOptions);
-
-      return [TextEdit.replace(Range.create(Position.create(0, 0), document.positionAt(text.length)), formatted)];
-    } catch (error) {
-      return [];
-    }
+    return []
   }
 }
